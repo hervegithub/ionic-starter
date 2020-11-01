6581485d850c5073b90c2a1a9c72f667632e3a0c
Hervé Yves <herveyves3@gmail.com>
2020-11-01 11:55:40 +0100

    Updating login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -9,9 +10,13 @@ export class LoginPage implements OnInit {
 
   isPasswordVisible: boolean = false;
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onLogin(){
+    this.router.navigateByUrl('home');
   }
 
   onShowPassword(){
